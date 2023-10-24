812609eb3127652bc1a3f8acb4210f478f757846
hussnain ishfaq <mh5589730@gmail.com>
2023-10-24 23:15:43 +0500

    m

--- lib/utils/about_utils.dart
+++
@@ -1,7 +0,0 @@
-class AboutUtils {
-  static const String aboutMeHeadline =
-      'I\'m Malik Hussnain, A Web Developer, "Shopify/wordpress Expert".';
-
-  static const String aboutMeDetail =
-      'Greetings! I\'m Malik Hussnain, boasting five years of experience in developing online stores and websites using WordPress, and Shopify. I\'m consistently connected to the internet for project efficiency and hold a degree in software engineering. For exceptional results, consider hiring my services.I specialize in crafting effective marketing and sales funnels tailored for Shopify online stores. When you collaborate with me, expect a Shopify or WordPress e-commerce website that not only looks great but is also optimized to boost conversions with unwavering certainty.';
-}
